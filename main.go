9d2a0ea4e40bd6d3b63e13ec8f9314dad0088b72
Tadashi Shigeoka <shigeoka@codenote.net>
2019-06-04 23:03:09 +0900

    Set HTTPPort, HTTPSPort from os.Getenv("PORT")
    - https://stackoverflow.com/questions/49896611/how-can-i-deploy-beego-app-to-heroku
    - https://beego.me/docs/mvc/controller/config.md

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	_ "github.com/codenote-net/beego-sandbox/routers"
+	"log"
+	"os"
+	"strconv"
+
 	"github.com/astaxie/beego"
+	_ "github.com/codenote-net/beego-sandbox/routers"
 )
 
 func main() {
+	log.Println("Env $PORT :", os.Getenv("PORT"))
+	if os.Getenv("PORT") != "" {
+		port, err := strconv.Atoi(os.Getenv("PORT"))
+		if err != nil {
+			log.Fatal(err)
+			log.Fatal("$PORT must be set")
+		}
+		log.Println("port : ", port)
+		beego.BConfig.Listen.HTTPPort = port
+		beego.BConfig.Listen.HTTPSPort = port
+	}
+
 	beego.Run()
 }
-
